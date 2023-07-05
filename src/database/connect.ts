a854dad492e552e3c459a2572ac9287612cca8e0
thomasop <tdss33@hotmail.com>
2023-07-05 17:46:48 +0200

    Edit

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -12,3 +12,27 @@ try {
 }
 
 export default connect;
+
+/* build-publish:
+    needs: test
+    runs-on: ubuntu-latest
+    environment: gcp
+    name: build docker image and push it to the GAR
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          ref: ${{ github.event.push.head.ref }}
+      - name: build code
+        uses: actions/setup-node@v3
+        with:
+          node-version: 18
+      - run: npm ci
+      - run: npm run build
+      - name: Login to GAR
+        uses: docker/login-action@v2
+        with:
+          registry: europe-west1-docker.pkg.dev/auth-391914/auth
+          username: _json_key
+          password: ${{ secrets.GCP_REGISTRY_KEY }}
+      - run: docker build -t auth/devops-workshop-api:${{ github.sha }} .
+      - run: docker push auth/devops-workshop-api:${{ github.sha }} */
