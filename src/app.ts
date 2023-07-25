db8a95d49b001c43400e735a84a866c218cff7e8
thomasop <tdss33@hotmail.com>
2023-07-25 07:25:54 +0200

    edit

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,43 @@
 import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
 import { Server } from "socket.io";
 import { createServer } from "http";
-import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import userRouter from "./routes/User.js";
 dotenv.config();
 
 let app = express();
 export const server = createServer(app);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors({ credentials: true, origin: "https://test-auth-alpha.vercel.app" }));
-app.use(cookieParser('test'));
-
-app.use("/user", userRouter);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+let users: any = {};
+io.on("connection", (socket) => {
+  console.log("user connection");
+  socket.on("send-msg", (data) => {
+    socket.broadcast.emit("get-msg", data.result);
+  });
+  socket.on("send-edit-new-msg", (data) => {
+    socket.broadcast.emit("get-edit-new-msg", data.result);
+  });
+  socket.on("send-log", function (data) {
+    users[socket.id] = data.userId;
+  });
+  socket.on("del-log", function (data) {
+    delete users[socket.id];
+  });
+  socket.broadcast.emit("get-log", users);
+  socket.on("send-conv", (data) => {
+    socket.broadcast.emit("get-conv", data.result);
+  });
+  socket.on("send-writing", (data) => {
+    socket.broadcast.emit('get-writing', data)
+  })
+  socket.on("disconnect", () => {
+    console.log("user disconnect");
+  });
+});
 
 export default app;
--- src/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-import dotenv from "dotenv";
-dotenv.config();
-
-export default (req: any, res: Response, next: NextFunction) => {
-
-  if (req.session.user) next();
-  else return res.status(401).json({ status: 401, errors: "no access" });
-};
